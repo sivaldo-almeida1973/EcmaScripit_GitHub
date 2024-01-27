e650bd1331609526791a57da6cab25f548a4f00b
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-01-27 10:50:04 -0300

    Covertendo objeto lietral para JSON e enviando para local storage

--- projetoOrçamentoPessoal/app.js
+++ projetoOrçamentoPessoal/app.js
@@ -1,5 +1,17 @@
-function cadastrarDespesas() {//ligada ao botao index.html
-  //criar uma variavel para referenciar
+class Despesa {
+  constructor(ano, mes, dia, tipo, descricao, valor) {
+    this.ano = ano
+    this.mes = mes
+    this.dia = dia
+    this.tipo = tipo
+    this.descricao = descricao
+    this.valor = valor
+  }//classe que gera o obj  despesa
+}
+
+//ligada ao botao index.html
+function cadastrarDespesas() {
+  //criar var para referenciar os valores dos campos
   let ano = document.getElementById('ano')
   let mes = document.getElementById('mes')//.value
   let dia = document.getElementById('dia')
@@ -7,7 +19,34 @@ function cadastrarDespesas() {//ligada ao botao index.html
   let descricao = document.getElementById('descricao')
   let valor = document.getElementById('valor')
 
-  //recupera os dados
-  console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
-  // console.log(mes)
+    //instancia da classe Despesa
+  let despesa = new Despesa(
+    ano.value,
+    mes.value,
+    dia.value, 
+    tipo.value,
+    descricao.value,
+    valor.value
+)
+//funcao que liga ao local storage
+gravar(despesa)
+
 }
+//funcao que liga ao local storage
+function gravar(d) {
+  //converter o arquivo (despesa) para JSON para enviar ao local storage
+  localStorage.setItem('despesa', JSON.stringify(d))
+}
+
+//JSON.stringify converte obj literal para JSON
+
+//JSON.parse converte de JSON para ob literal
+
+
+
+
+
+
+
+
+
