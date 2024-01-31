2ba9251edba862a910e4cb6d1ae06828debb99f2
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-01-31 13:20:59 -0300

    Listando despesas

--- projetoOrçamentoPessoal/app.js
+++ projetoOrçamentoPessoal/app.js
@@ -48,6 +48,30 @@ class Bd {
       localStorage.setItem('id' ,id )
 
     }
+   
+    recuperarTodosRegistros() {
+      //array de despesas
+      //a cada intera vou pegar a despesa no local storage
+      let despesas = Array()
+
+      let id = localStorage.getItem('id')
+       //vai recuperar todas as despesas em local cadastradas storage
+      for(let i = 1; i <= id ; i++) {
+        //recuperar a desepesa(e converter para obj literal)
+        let despesa = JSON.parse(localStorage.getItem(i))
+        
+        //se existe existe removidos
+        //pular esses indices
+        if(despesa === null) {
+          continue
+        }
+
+        despesas.push(despesa)
+
+      }
+
+        return despesas
+    }
   
 }
 //chamar instancia da class Bd------------------------------------
@@ -76,16 +100,38 @@ function cadastrarDespesas() {
 
      //chamada o obj da classe bd
     if(despesa.validarDados()) {//se os dados forem validos
-       bd.gravar(despesa)
+       //bd.gravar(despesa)
+
+       document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso!'
+       document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+       document.getElementById('modal_conteudo').innerHTML = 'Despesa foi Cadastrada com sucesso!'
+       document.getElementById('modal_btn').innerHTML = 'Voltar'
+       document.getElementById('modal_btn').className = 'btn btn-success'
+       
        //dialog de sucesso
-       $('#sucessoGravacao').modal('show')
+       $('#modalRegistraDespesa').modal('show')
     }else {
+
       //dialog de erro
-      $('#erroGravacao').modal('show')
+      document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro!'
+      document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+      document.getElementById('modal_conteudo').innerHTML = 'Erro de gravação, verifique se todos os compos foram preenchidos corretamente!'
+      document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+      document.getElementById('modal_btn').className = 'btn btn-danger'
+
+      $('#modalRegistraDespesa').modal('show')
 
     }
 }
 
+//---------onload="carregaListaDsepesas()" no body-------------------------------------
+function carregaListaDespesas() {
+  let despesas = Array()
+
+  despesas =  bd.recuperarTodosRegistros()
+
+  console.log(despesas)
+}
 
 
 
